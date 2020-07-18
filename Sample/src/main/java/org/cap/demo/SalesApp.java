9a9fadb954d42b629aeb51e5a69443045dd2d5e7
viji <vijayakshmi.david@capgemini.com>
2020-07-18 12:38:01 +0530

    loop updated with odd and even

--- Sample/src/main/java/org/cap/demo/SalesApp.java
+++ Sample/src/main/java/org/cap/demo/SalesApp.java
@@ -5,12 +5,14 @@ public class SalesApp {
 	int[] arr= {12,34,56,78,89};
 	
 	public void printdata() {
-		int sum=0;
+		int sumeven=0,sumodd=0;
 		for(int num:arr) {
-			//sum+=num;
-			sum=sum+num;
-			System.out.println(num);
+			if(num%2==0)
+				sumeven+=num;
+			else 
+				sumodd+=num;
 		}
-		System.out.println("Sum:" + sum);
+		System.out.println("Even Sum:" + sumeven);
+		System.out.println("Odd Sum:" + sumodd);
 	}
 }
